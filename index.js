29fb37899b900ea297794f08f3efbfa88e8e5f73
+ <+>
2024-03-19 18:03:02 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1= 31; 
+const num2= 2;
+const multiply = num1 * num2;
 
-// Write your code here
+function randomNumberGreaterThanZero() {
+    return Math.floor(Math.random() * 6) + 1;
+
+}
+const random = randomNumberGreaterThanZero();
+
+const num3= 14;
+const num4= 5;
+const mod = num3 % num4;
+
+const numbers = [3,5,10,12,15,17,20];
+const max = Math.max (...numbers);
